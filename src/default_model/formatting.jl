41e317fe032fd8b16b8be029046c729ab61acbf5
Nicolas Wink <nicolas.wink@tu-darmstadt.de>
2024-04-03 15:53:51 +0200

    ...

--- src/default_model/formatting.jl
+++ src/default_model/formatting.jl
@@ -2,4 +2,4 @@ function parameterString(params::AbstractParameter)
     join([string(p) * "_" * cfmt("%.3f", getfield(par, p)) for p in fieldnames(typeof(params))], "__")
 end
 
-saveDict(p::AbstractParameter{T}) = throw(MissingException("Please provide an implementation by overwriting this function."))
+saveDict(p::AbstractParameter{T}) where {T} = throw(MissingException("Please provide an implementation by overwriting this function."))
